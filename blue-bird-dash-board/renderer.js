438f9a3fa9b3f57ac503684aa57ca880c482a956
YeonSeong-Lee <abc@def.com>
2024-09-12 15:28:37 +0900

    Refactor CSS and HTML for bottom row styling

--- blue-bird-dash-board/renderer.js
+++ blue-bird-dash-board/renderer.js
@@ -18,6 +18,33 @@ const fetchMenu = async() => {
   wed_launch.innerText = wed_menu.join('\n');
   thu_launch.innerText = thu_menu.join('\n');
   fri_launch.innerText = fri_menu.join('\n');
+
+  const today = new Date().toLocaleDateString('kr', { weekday: 'long' });
+  console.log(today);
+  switch (today) {
+    case '월요일':
+      mon_launch.style.fontWeight = 'bold';
+      document.getElementById('mon-bottom').style.backgroundColor = 'red';
+      break;
+    case '화요일':
+      tue_launch.style.fontWeight = 'bold';
+      document.getElementById('tue-bottom').style.backgroundColor = 'red';
+      break;
+    case '수요일':
+      wed_launch.style.fontWeight = 'bold';
+      document.getElementById('wed-bottom').style.backgroundColor = 'red';
+      break;
+    case '목요일':
+      thu_launch.style.fontWeight = 'bold';
+      document.getElementById('thu-bottom').style.backgroundColor = 'red';
+      break;
+    case '금요일':
+      fri_launch.style.fontWeight = 'bold';
+      document.getElementById('fri-bottom').style.backgroundColor = 'red';
+      break;
+    default:
+      break;
+  }
   
 }
 

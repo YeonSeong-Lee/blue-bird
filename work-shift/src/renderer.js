bc7397382bf95dc69957ab4f20aa498b9b923b0b
YeonSeong-Lee <abc@def.com>
2024-09-19 16:42:13 +0900

    feat: parse and process raw data from fetched xlsx file
    This commit adds functionality to parse and process the raw data obtained from the fetched xlsx file. It validates the data, extracts the year, and generates formatted dates. It also creates a parsed data object with the name and corresponding values for each date. The commit includes console logs for debugging purposes.
    
    Refactor index.js: Comment out opening DevTools

--- work-shift/src/renderer.js
+++ work-shift/src/renderer.js
@@ -1,7 +1,32 @@
 const fetch_xlsx = async () => {
-    const raw_data = await window.versions.fetch_xlsx();
-    console.log(raw_data);
-    
+    const raw_data = await window.versions.fetch_xlsx()
+    // TODO: validate raw_data and alert if it's invalid
+    const year = '20' + raw_data[0].split('-')[0]
+    console.log(raw_data)
+    const date = []
+    for (let i = 1; i < raw_data[1].length; i++) {
+        if (raw_data[1][i].value === null || raw_data[2][i].value === null) {
+            continue
+        }
+        date.push(year + '년 ' + raw_data[1][i].value + ' ' + raw_data[2][i].value + '일')
+    }
+    const parsed_data = {}
+    for (let i = 4; i < raw_data.length; i++) {
+        const row = []
+        const name = raw_data[i][1]?.value
+        for (let j = 2; j < raw_data[i].length; j++) {
+            if (!raw_data[i][j] || !raw_data[i][j].value) {
+                continue
+            }
+            const temp = { date: date[j - 2], value: raw_data[i][j].value }
+            if (raw_data[i][j].style?.fill?.fgColor?.argb === 'FFFFFF00') {
+                temp['노D'] = true
+            }
+            row.push(temp)
+        }
+        parsed_data[name] = row
+    }
+    console.log(parsed_data)
 }
 
 fetch_xlsx();

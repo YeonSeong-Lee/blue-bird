2843dceb96049e007e51749ea96d95541f6fcd87
YeonSeong-Lee <abc@def.com>
2024-09-30 15:24:53 +0900

    chore: delete console.log

--- work-shift/src/work-shift.js
+++ work-shift/src/work-shift.js
@@ -7,19 +7,12 @@ class WorkShift extends HTMLElement {
         // TODO: should be fetch after render.js is loaded
         const parsed_data_by_name = JSON.parse(localStorage.getItem('parsed_data_by_name'));
         const parsed_data_by_date = JSON.parse(localStorage.getItem('parsed_data_by_date'));
-        console.log('parsed_data_by_name', parsed_data_by_name);
-        console.log('parsed_data_by_date', parsed_data_by_date);
         const today_key = new Date().toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' });
-        console.log('data', parsed_data_by_date[today_key]);
         const day_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('D'));
         const evening_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('E'));
         const night_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('N'));
         const yellow_day_workers = parsed_data_by_date[today_key].filter(worker => worker["노D"] === true);
 
-        console.log('day_worker', day_worker);
-        console.log('yellow_day_workers', yellow_day_workers);
-        console.log('evening_worker', evening_worker);
-        console.log('night_worker', night_worker);
 
         this.shadowRoot.innerHTML = `
             <div class="work-shift">

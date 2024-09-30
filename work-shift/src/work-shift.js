c401f3266cef6f10e38b947866ecb8df453e071f
YeonSeong-Lee <abc@def.com>
2024-09-30 16:38:07 +0900

    feat: 사용자 입력 받기
    입력에 따라 다른 근무표를 보여주기 위해

--- work-shift/src/work-shift.js
+++ work-shift/src/work-shift.js
@@ -5,6 +5,10 @@ class WorkShift extends HTMLElement {
         const today = new Date().toLocaleDateString('kr', {month: '2-digit', day: "2-digit"});
         // TODO: get data from localStorage,sync with the latest data
         // TODO: should be fetch after render.js is loaded
+
+        // TODO: 날짜를 입력받아서 해당 날짜의 근무자를 출력하도록 수정
+        // TODO: work-shift를 여러개 설정
+        // TODO: 날짜 input으로 입력 받을 수 있도록.
         const parsed_data_by_name = JSON.parse(localStorage.getItem('parsed_data_by_name'));
         const parsed_data_by_date = JSON.parse(localStorage.getItem('parsed_data_by_date'));
         const today_key = new Date().toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' });
@@ -57,12 +61,26 @@ class WorkShift extends HTMLElement {
                 background-color: #f1f1f1;
             }`;
 
+
+        // TODO: 사용자의 선택에 따라 근무자를 출력하도록 수정
         this.shadowRoot.innerHTML = `
             <style>${css}</style>
             <div class="work-shift">
                 <table>
                 <tr>
                     <th colspan="2" id="header">${today} 근무표</th>
+                    <tr>
+                        <td colspan="2">
+                        <form id="shift-form">
+                            <input type="checkbox">사무국</input>
+                            <input type="checkbox">건강증진과</input>
+                            <input type="checkbox">생활복지과</input>
+                            <input type="checkbox">재활사업과</input>
+                            <input type="date" id="date-input" />
+                            <input type="submit" id="load-shift" value="근무표 불러오기" />
+                        </form>
+                        </td>
+                    </tr>
                 <tr>
                     <th>시간</th>
                     <th>근무자</th>

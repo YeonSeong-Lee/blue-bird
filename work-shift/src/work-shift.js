b54e19e16859576c18f0ab3cb08647daa879d807
YeonSeong-Lee <abc@def.com>
2024-09-30 15:58:50 +0900

    feat: work-shift에 css 추가

--- work-shift/src/work-shift.js
+++ work-shift/src/work-shift.js
@@ -12,12 +12,57 @@ class WorkShift extends HTMLElement {
         const evening_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('E'));
         const night_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('N'));
         const yellow_day_workers = parsed_data_by_date[today_key].filter(worker => worker["노D"] === true);
+        const css = `table {
+                width: 100%;
+                border-collapse: collapse;
+                margin: 20px 0;
+                font-size: 18px;
+                text-align: left;
+            }
+            
+            #header {
+                background-color: #73a7d9;
+                color: #ffffff;
+                font-size: 24px;
+                font-weight: bold;
+                text-align: center;
+            }
 
+            table thead tr {
+                background-color: #73a7d9;
+                color: #ffffff;
+                text-align: left;
+                font-weight: bold;
+            }
+
+            table th, table td {
+                padding: 12px 15px;
+                border: 1px solid #dddddd;
+                min-width: 42px;
+            }
+
+            table tbody tr {
+                border-bottom: 1px solid #dddddd;
+            }
+
+            table tbody tr:nth-of-type(even) {
+                background-color: #f3f3f3;
+            }
+
+            table tbody tr:last-of-type {
+                border-bottom: 2px solid #73a7d9;
+            }
+
+            table tbody tr:hover {
+                background-color: #f1f1f1;
+            }`;
 
         this.shadowRoot.innerHTML = `
+            <style>${css}</style>
             <div class="work-shift">
-                <h2>${today} 근무표</h2>
                 <table>
+                <tr>
+                    <th colspan="2" id="header">${today} 근무표</th>
                 <tr>
                     <th>시간</th>
                     <th>근무자</th>

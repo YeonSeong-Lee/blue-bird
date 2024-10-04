3275f38f887f7d923ae07e9f507d10672c0910d4
YeonSeong-Lee <abc@def.com>
2024-10-04 16:21:03 +0900

    feat: syncDate

--- work-shift/src/work-shift.js
+++ work-shift/src/work-shift.js
@@ -4,24 +4,35 @@ class WorkShift extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        const today = new Date().toLocaleDateString('kr', {month: '2-digit', day: "2-digit"});
-        // TODO: get data from localStorage,sync with the latest data
-        // TODO: should be fetch after render.js is loaded
 
-        // TODO: 날짜를 입력받아서 해당 날짜의 근무자를 출력하도록 수정
-        // TODO: work-shift를 여러개 설정
-        // TODO: 날짜 input으로 입력 받을 수 있도록.
-        const parsed_data_by_name = JSON.parse(localStorage.getItem('parsed_data_by_name'));
+        const style = document.createElement('style');
+        style.textContent = css;
+        this.shadowRoot.appendChild(style);
+
+        const container = document.createElement('div');
+        container.classList.add('container');
+        this.shadowRoot.appendChild(container);
+
+        this.render(new Date().toISOString().slice(0, 10));
+    }
+
+    syncDate(event) {
+        const selectedDate = event.target.value;
+        console.log(selectedDate);
+        this.render(selectedDate);
+    }
+
+    render(date) {
+        const today = new Date(date).toLocaleDateString('kr', { month: '2-digit', day: "2-digit" });
         const parsed_data_by_date = JSON.parse(localStorage.getItem('parsed_data_by_date'));
-        const today_key = new Date().toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' });
-        const day_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('D') && !worker["노D"]);
-        const yellow_day_workers = parsed_data_by_date[today_key].filter(worker => worker.value.includes('D') && worker["노D"] === true);
-        const evening_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('E'));
-        const night_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('N'));
-        const off_worker = parsed_data_by_date[today_key].filter(worker => worker.value.includes('off'));
+        const today_key = new Date(date).toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' });
+        const day_worker = parsed_data_by_date[today_key]?.filter(worker => worker.value.includes('D'));
+        const yellow_workers = parsed_data_by_date[today_key]?.filter(worker => worker["노D"] === true);
+        const evening_worker = parsed_data_by_date[today_key]?.filter(worker => worker.value.includes('E'));
+        const night_worker = parsed_data_by_date[today_key]?.filter(worker => worker.value.includes('N'));
+        const off_worker = parsed_data_by_date[today_key]?.filter(worker => worker.value.includes('off'));
 
-        // TODO: 사용자의 선택에 따라 근무자를 출력하도록 수정
-        this.shadowRoot.innerHTML = `
+        this.shadowRoot.querySelector('.container').innerHTML = `
             <style>${css}</style>
             <div class="work-shift">
                 <table>
@@ -29,8 +40,8 @@ class WorkShift extends HTMLElement {
                     <th colspan="2" id="header">${today} 근무표</th>
                     <tr>
                         <td colspan="2">
-                        <form id="shift-form" >
-                            <input type="date" id="date-input" value=${new Date().toISOString().slice(0, 10)}  onchange="alert(this.value)"/>
+                        <form id="shift-form">
+                            <input type="date" id="date-input" value=${date} />
                             <input type="reset" id="reset-shift" value="오늘 근무" />
                         </form>
                         </td>
@@ -41,29 +52,31 @@ class WorkShift extends HTMLElement {
                 </tr>
                 <tr>
                     <td>노D</td>
-                    <td>${yellow_day_workers.map((e) => e.name)}</td>
+                    <td>${yellow_workers.map((e) => e.name).join(', ')}</td>
                 </tr>
                 <tr>
                     <td>D</td>
-                    <td>${day_worker.map((e) => e.name)}</td>
+                    <td>${day_worker.map((e) => e.name).join(', ')}</td>
                 </tr>
                 <tr>
                     <td>E</td>
-                    <td>${evening_worker.map((e) => e.name)}</td>
+                    <td>${evening_worker.map((e) => e.name).join(', ')}</td>
                 </tr>
                 <tr>
                     <td>N</td>
-                    <td>${night_worker.map((e) => e.name)}</td>
+                    <td>${night_worker.map((e) => e.name).join(', ')}</td>
                 </tr>
                 <tr>
-                    <td>off</td>
-                    <td>${off_worker.map((e) => e.name)}</td>
+                    <td>Off</td>
+                    <td>${off_worker.map((e) => e.name).join(', ')}</td>
                 </tr>
                 </table>
             </div>
         `;
+        this.shadowRoot.querySelector('#date-input').addEventListener('change', (event) => {
+            this.syncDate(event);
+        });
     }
-
 }
 
 customElements.define('work-shift', WorkShift);
